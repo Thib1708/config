d500a2c13deb2116948ec6d0c5ca4eb2038b3971
Thib1708 <thibault.giraudon@gmail.com>
2023-02-25 23:11:01 +0100

    Fifth commit : test

--- config.sh
+++ config.sh
@@ -2,7 +2,7 @@
 
 RC_FILE="$HOME/.zshrc"
 
-git clone 
+git clone https://github.com/Thib1708/config.git
 
 printf "installing zsh-autosuggestion\n"
 git clone https://github.com/zsh-users/zsh-autosuggestions ~/.zsh/zsh-autosuggestions
@@ -27,34 +27,7 @@ alias gcl='git clone'\n
 " >> "$RC_FILE"
 
 printf "installing print_news\n"
-cd ~
-rm -rf print_news
-
-git clone https://github.com/Thib1708/print_news.git
-
-cd ./print_news
-make 
-mkdir articles
-chmod 751 ./bin/main.sh
-cd ..
-
-cp -r print_news "$HOME"
-
-cd "$HOME" || exit
-rm -rf temp_____
-
-# set up the alias
-if ! grep "SUBJECT=" "$RC_FILE" &> /dev/null; then
-	echo "cmds not present"
-	printf "\nexport SUBJECT=\"Apple\"\n
-export COUNTRY=\"fr\"\n
-export API_KEY=\"\"\n
-export CITY=\"Lyon\"\n
-alias news=%s/print_news/bin/main.sh\n
-news" "$HOME" >> "$RC_FILE"
-printf "\033[1;32mcmds add\033[0m\n"
-printf "\033[48;5;5mopen ~/.zshrc and copy/paste your api_key in export API_KEY=\"\"\033[0m\n"
-fi
-
+chmod 751 ~/config/install-print_news.sh
+bash ~/config/install-print_news.sh
 printf "print_news installation : DONE\n\n"
 
